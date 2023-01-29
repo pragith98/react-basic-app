e72ab7f4113255b4527d50e48894f7cd44de31ad
pragith98 <lakshanxp@hotmail.com>
2023-01-29 23:43:09 +0530

    create contacts page

---
+++ src/components/pages/Contacts.jsx
@@ -0,0 +1,12 @@
+import ContactsTable from "../contacts/ContactsTable";
+
+function Contacts() {
+  return (
+    <div className="container mt-5">
+      <h1 className="mb-4">Contacts</h1>
+      <ContactsTable/>
+    </div>
+  );
+}
+
+export default Contacts;
