5e690b499464462f76ea4d57631321d77e4b118c
pragith98 <lakshanxp@hotmail.com>
2023-02-05 23:14:58 +0530

    create name list page

---
+++ src/components/pages/NameList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function NameList() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <h1>Name List</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni at iure
+          natus exercitationem nesciunt dicta unde omnis asperiores, impedit
+          veniam facilis a culpa minima explicabo commodi architecto aut ducimus
+          quae.
+        </p>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default NameList;
