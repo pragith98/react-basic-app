d61b1881effdf368ed0c39b7306dcc0e06790466
pragith98 <lakshanxp@hotmail.com>
2023-02-05 23:15:42 +0530

    modify navigation menu items

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -26,45 +26,13 @@ function NavigationBar() {
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">
-                Features
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Pricing
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
                 About
               </a>
             </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                data-bs-toggle="dropdown"
-                href="#"
-                role="button"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Dropdown
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Name list
               </a>
-              <div className="dropdown-menu">
-                <a className="dropdown-item" href="#">
-                  Action
-                </a>
-                <a className="dropdown-item" href="#">
-                  Another action
-                </a>
-                <a className="dropdown-item" href="#">
-                  Something else here
-                </a>
-                <div className="dropdown-divider"></div>
-                <a className="dropdown-item" href="#">
-                  Separated link
-                </a>
-              </div>
             </li>
           </ul>
         </div>
