5f1c6bd444679045fbe10ded31aac3f31f77de07
pragith98 <lakshanxp@hotmail.com>
2023-02-05 22:55:04 +0530

    change alignments

--- src/components/contacts/ContactsTable.jsx
+++ src/components/contacts/ContactsTable.jsx
@@ -2,43 +2,45 @@ import { useEffect, useState } from "react";
 import ContactTableComponentItem from "./ContactTableComponentItem";
 
 function ContactsTable() {
-    const [loadData, setLoadData] = useState(false);
-    const [contactList, setContactList] = useState([]);
+  const [loadData, setLoadData] = useState(false);
+  const [contactList, setContactList] = useState([]);
 
-  useEffect(()=>{
-    fetch("https://www.randomuser.me/api").then(Response =>{
-        return Response.json()
-    }).then(Response =>{
+  useEffect(() => {
+    fetch("https://www.randomuser.me/api")
+      .then((Response) => {
+        return Response.json();
+      })
+      .then((Response) => {
         setContactList((contactList) => contactList.concat(Response.results));
-    })
-  },[loadData]);
-
-
+      });
+  }, [loadData]);
 
   const contactListComponentList = contactList.map((item) => {
-    return(
-        <ContactTableComponentItem
+    return (
+      <ContactTableComponentItem
         key={item.phone}
         name={`${item.name.title}. ${item.name.first} ${item.name.last}`}
         telephone={item.phone}
         photo={item.picture.thumbnail}
       />
-    )
+    );
   });
 
-  
-
-  
-
-  const addNewItem = () =>{
+  const addNewItem = () => {
     setLoadData(!loadData);
-  }
+  };
 
   return (
     <div>
       <h4>Contacts Table</h4>
-        <button type="button" className="btn btn-primary mb-1" onClick={addNewItem}>Add New</button>
-      
+      <button
+        type="button"
+        className="btn btn-primary mb-1"
+        onClick={addNewItem}
+      >
+        Add New
+      </button>
+
       <table className="table table-hover">
         <thead>
           <tr className="table-dark">
